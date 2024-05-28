5a50ac313ea8e4518a3d90022dbfb682e64e5ae0
djoleitcentar <djordje.krstic@praktikanti.itcentar.rs>
2024-05-28 10:26:05 +0200

    update

--- src/app/shared/components/form-builder/dropdown/dropdown.component.ts
+++ src/app/shared/components/form-builder/dropdown/dropdown.component.ts
@@ -6,12 +6,21 @@ import {
 } from '@angular/forms';
 import { Field } from '../../../models/index';
 import { NgClass, NgStyle } from '@angular/common';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatFormFieldModule } from '@angular/material/form-field';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass, NgStyle],
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    NgStyle,
+    // MatSelectModule,
+    // MatFormFieldModule,
+    NzSelectModule,
+  ],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss',
   viewProviders: [
--- src/app/shared/components/form-builder/form-input/form-input.component.ts
+++ src/app/shared/components/form-builder/form-input/form-input.component.ts
@@ -1,15 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Field } from '../../../models/index';
-import { ControlContainer, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlContainer,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { NgClass } from '@angular/common';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-form-input',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, NzInputModule, NzFormModule],
   templateUrl: './form-input.component.html',
   styleUrl: './form-input.component.scss',
-  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
+  viewProviders: [
+    { provide: ControlContainer, useExisting: FormGroupDirective },
+  ],
 })
 export class FormInputComponent {
   @Input() field: Field;
